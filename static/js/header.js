e0b559a5ceedc7fa7a3f4185c463e56c8d0394ea
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-06-27 00:28:34 +0400

    added experience part and animated header

--- static/js/header.js
+++ static/js/header.js
@@ -4,3 +4,13 @@ navMenuButton.addEventListener("click", function(event){
     console.log("hello");
 })
 
+const header = document.querySelector("header");
+const toggleClass = "is-sticky";
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  if (currentScroll > 100) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+})
