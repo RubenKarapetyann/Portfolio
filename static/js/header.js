5a6b5e104273a93d4d65d700d4ca1e95c3d1e3cf
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-07-02 17:36:52 +0400

    added mobile header with its animations, styles and scripts

--- static/js/header.js
+++ static/js/header.js
@@ -1,7 +1,37 @@
 const navMenuButton = document.getElementById("nav-menu-button")
+const mobileNav = document.getElementById("mobile-nav")
+const mobileNavElements = document.getElementsByClassName("mobile-element-inner-container")
+
+function navClosing(){
+  mobileNav.classList.remove("isOpening")
+  mobileNav.classList.add("isClosing")
+  navMenuButton.classList.remove("isTurning90")
+  navMenuButton.classList.add("isTurning-90")
+  setTimeout(()=>{
+    mobileNav.style.display = "none"
+  }, 300)
+  navMenuButton.style.transform = "rotate(0deg)"
+}
+function navOpening(){
+  mobileNav.style.display = "block"
+  navMenuButton.style.transform = "rotate(90deg)"
+  navMenuButton.classList.remove("isTurning-90")
+  navMenuButton.classList.add("isTurning90")
+  mobileNav.classList.remove("isClosing")
+  mobileNav.classList.add("isOpening")
+}
+
+for(let i = 0; i < mobileNavElements.length; i++){
+  const el = mobileNavElements.item(i)
+  el.addEventListener("click", navClosing)
+}
 
 navMenuButton.addEventListener("click", function(event){
-    console.log("hello");
+  if(mobileNav.style.display === "block"){
+    navClosing()
+  }else {
+    navOpening()
+  }
 })
 
 const header = document.querySelector("header");
